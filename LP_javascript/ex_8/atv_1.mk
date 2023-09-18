450b33fd883d75dfd44cb3fb6a9d5534f8e86f4c
MoonAmon <misterofclans.mo@gmail.com>
2023-09-18 11:19:11 -0300

    atv CPI II

---
+++ LP_javascript/ex_8/atv_1.mk
@@ -0,0 +1,68 @@
+Para essas atividades utilize somente JavaScript para obter os dados do usuário (prompt) e faça a chamada da função diretamente no código, como no exemplo do slide 11 da Aula Funções (parte 2).
+
+1) Criar um algoritmo que produza a saída a seguir, usando uma função sem retorno de valor. Exemplo de função sem retorno de valor: slide 11 da aula Funções (parte 2). No exemplo não é utilizado o comando return para retornar valor.
+
+***************
+
+ANITA LOPES
+
+===============
+
+GUTO GARCIA
+
+###############
+
+2) Criar um algoritmo que possa entrar com três números e, para cada um, imprimir o dobro. Usar uma função que retorne valor. Exemplo de função que retorne valor: slide 13 da aula Funções (parte 2). No exemplo é utilizado o comando return para retornar um valor.
+
+Exemplo de saída:
+
+Informe um numero: 12
+
+dobro: 24
+
+Informe um numero: 45
+
+dobro: 90
+
+Informe um numero: 78
+
+dobro: 156
+
+3) Criar um algoritmo que possa entrar com três números e, para cada um, imprimir o dobro. Usar duas funções. Para esse exercício, você terá uma função que irá calcular o dobro do valor fornecido pelo usuário, e uma outra função que pode por exemplo receber como parâmetro um caractere e imprimir na tela, como é mostrado no exemplo de saída.
+
+Exemplo de saída:
+
+********************
+
+Informe um numero: 12
+dobro: 24
+
+********************
+
+Informe um numero: 36
+
+dobro: 72
+
+********************
+
+Informe um numero: 67
+
+dobro: 134
+
+********************
+
+4) Criar um algoritmo que receba notas de três provas e calcular a sua média, mas utilizando uma função. Para esse exercício utilize função com parâmetro, como no exemplo do slide 13 da aula Funções (parte 2). A diferença é que neste exercício a sua função terá 3 parâmetros, que são as notas informadas pelo usuário.
+
+Exemplo de saída:
+
+digite a nota 1: 5
+
+digite a nota 2: 6
+
+digite a nota 3: 7
+
+media aritmética = 6.00
+
+5) Criar uma função que converta radianos para graus.
+
+6) Envie os exemplos da aula.
