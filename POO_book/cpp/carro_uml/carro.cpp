7927f63ab46141a4a4b86a065eeb51656c6ca268
MoonAmon <misterofclans.mo@gmail.com>
2023-10-06 20:59:04 -0300

    poo book

--- POO_book/cpp/carro_uml/carro.cpp
+++ POO_book/cpp/carro_uml/carro.cpp
@@ -3,10 +3,64 @@
 #include <exception>
 #include "carro.h"
 
+using namespace std;
+
 Carro::Carro(){
     this->modelo = "Fusca";
     this->ano = 1981;
     this->preco = 5000;
     this->cor = "Azul";
-    this-
+    totalCarros++;
+    cout << "Método construtor chamado.";
+    cout << "Nenhum modelo configurado, este será um Fusca" << endl;
+}
+
+Carro::Carro(string modelo, int ano, float preco, string cor) {
+    this->modelo = modelo;
+    this->ano = ano;
+    this->preco = preco;
+    this->cor = cor;
+    totalCarro++;
+    cout << "Método construtor do carro " << this->modelo << endl;
+}
+
+Carro::~Carro() {
+    totalCarro--;
+    cout << "Método destrutor chamado para o carro "<< this->modelo << endl;
+}
+
+string Carro::getModelo() {
+    return this->modelo;
+}
+
+void Carro::setModelo(string novo_modelo) {
+    this->modelo = novo_modelo;
+}
+
+string Carro::getCor() {
+    return this->cor;
+}
+
+void Carro::setCor(string nova_cor) {
+    this->cor = nova_cor;
+}
+
+int Carro::getAno() {
+    return this->ano;
+}
+
+void Carro::setAno(string novo_ano) {
+    try
+    {
+        this->setAno(stoi(novo_ano));
+    }
+    catch(exception e) {}
+    
+}
+
+void Carro::setAno(int novo_ano) {
+    this->ano = novo_ano;
+}
+
+float Carro::getPreco() {
 }
