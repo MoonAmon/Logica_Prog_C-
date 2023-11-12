99033715d4dc44732781f521cdbfef9fc8e0a6bd
MoonAmon <misterofclans.mo@gmail.com>
2023-11-12 10:53:29 -0300

    Atividade de revisão 2 CPI II

--- LP_javascript/arquivos-iniciais11nov/js/calculaCompra.js
+++ LP_javascript/arquivos-iniciais11nov/js/calculaCompra.js
@@ -1,17 +1,44 @@
 const botaoCalcula = document.getElementById("calcula");
+const meuModal = document.getElementById("meuModal");
+const close = document.getElementsByClassName("close")[0];
 
 botaoCalcula.addEventListener("click", () => {
     let compras = [];
     camposTexto.forEach((campo) => {
-        if(campo.value) { compras.push({
-            nome: campo.name,
-            preco: campo.getAttribute("data-preco"),
-            quantidade: campo.value
-        });
-    }
+        if(campo.value) { 
+            compras.push({
+                nome: campo.name,
+                preco: campo.getAttribute("data-preco"),
+                quantidade: campo.value
+            });
+        }
     });
 
     if(compras.length > 0) {
-        abreNovaJanela(compras);
+        let compraTexto = '<table><tr><th>Quantidade</th><th>Nome</th><th>Preço Total</th></tr>';
+        let total = 0;
+        compras.forEach(compra => {
+            let preco = compra.quantidade * compra.preco;
+            total += preco;
+            compraTexto += `<tr>
+                                <td>${compra.quantidade} kg</td> 
+                                <td>${compra.nome}</td> 
+                                <td>R$ ${preco.toFixed(2)}</td>
+                            </tr>
+                            `;
+        });
+        compraTexto += `<td colspan="2">Total</td><td>R$ ${total.toFixed(2)}</td></tr></table>`
+        document.getElementById("resultado").innerHTML = compraTexto;
+        meuModal.style.display = "block";
     }
 });
+
+close.onclick = function() {
+    meuModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == meuModal) {
+        meuModal.style.display = "none";
+    }
+}
--- LP_javascript/arquivos-iniciais11nov/js/validaCampo.js
+++ LP_javascript/arquivos-iniciais11nov/js/validaCampo.js
@@ -13,8 +13,13 @@ function validaCampo(campo) {
    spanErro.innerHTML = ""; 
    campo.classList.remove("foco");
 
-   if(campo.validity.patternMismatch) {
-       spanErro.innerHTML = "Preencha um número inteiro ou decimal!";
+   if (campo.validity.valueMissing) {
+       spanErro.innerHTML = "Preencha um valor válido!";
+       campo.classList.add("foco");
+    
+   }
+   else if(campo.validity.patternMismatch) {
+       spanErro.innerHTML = "Preencha um número ou decimal!";
        campo.classList.add("foco");
    }
 }
